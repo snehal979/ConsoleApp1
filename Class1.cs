9abcf46ca96304dda9b412148f7cb305221afca9
Snehal979 <bansodsnehal535@gmail.com>
2023-08-16 01:52:33 +0530

    demo 2

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        public static void Number()
+        {
+            Console.WriteLine(3+7);
+        }
+    }
+}
